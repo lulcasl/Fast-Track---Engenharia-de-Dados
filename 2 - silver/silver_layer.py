980aeb4b5ce567044ef3f54872f735b6af0fcd5b
Lucas Vinícius dos Santos <lucasv1993@gmail.com>
2026-02-25 22:21:48 -0300

    Finalizando a camada silver

--- 2 - silver/silver_layer.py
+++ 2 - silver/silver_layer.py
@@ -3,7 +3,7 @@ import json
 import os
 
 # ====================================================================
-# Etapa 1: Ler o arquivo criado na camada bronze.
+# Etapa 1: Ler o arquivo criado na camada bronze
 # ====================================================================
 
 # Definir o arquivo criado na camada anterior.
@@ -20,7 +20,7 @@ print("\n", f"Total de linhas: {len(df_silver)}")
 print(f"Total de colunas: {len(df_silver.columns)}", "\n")
 
 # ====================================================================
-# Etapa 2: Normalizar todos os campos aninhados.
+# Etapa 2: Normalizar todos os campos aninhados
 # ====================================================================
 
 # Verificar os dados existentes, pra poder normalizar os campos e unir
@@ -87,7 +87,7 @@ print("\n", f"Total de linhas: {len(df_silver)}")
 print(f"Total de colunas: {len(df_silver.columns)}", "\n")
 
 # ====================================================================
-# Etapa 3: Ajustar a tipagem de dados.
+# Etapa 3: Ajustar a tipagem de dados
 # ====================================================================
 
 # errors = coerce transformar os erros em NaT (Nott a Time), que é o equivalente a NaN para datas
@@ -100,9 +100,46 @@ print(df_silver[['timestamp_created_at', 'timestamp_updated_at']].dtypes)
 print()
 
 # ====================================================================
-# Etapa 4: Limpeza.
+# Etapa 4: Limpeza
 # ====================================================================
 
 # Verificar total ANTES da limpeza
 print(f"Total de registros ANTES da limpeza: {len(df_silver)}")
 print()
+
+# Verificar total de NaT - sum() soma tudo que é True.
+nat_created = df_silver['timestamp_created_at'].isna().sum()
+nat_updated = df_silver['timestamp_updated_at'].isna().sum()
+
+print(f"Total de registros com timestamp_created_at NaT: {nat_created}")
+print(f"Total de registros com timestamp_updated_at NaT: {nat_updated}", '\n')
+
+# Verificar issues resolvidos SEM data de resolução
+resolved_issues_nat = df_silver['issue_status'].isin(['Done', 'Resolved']) & df_silver['timestamp_updated_at'].isna()
+
+print(f"Total de issues resolvidos ou finalizados sem data de atualização: {resolved_issues_nat.sum()}")
+
+df_silver = df_silver[df_silver['timestamp_created_at'].notna()]
+df_silver = df_silver.reset_index(drop=True)
+# Se ele foi done ou resolved, precisa ter uma data do update do card, do contrário é inconsistência.
+resolved_issues_nat = df_silver['issue_status'].isin(['Done', 'Resolved']) & df_silver['timestamp_updated_at'].isna()
+df_silver = df_silver[~resolved_issues_nat]
+df_silver = df_silver.reset_index(drop=True)
+
+# Verificar total DEPOIS da limpeza
+print("\nStatus distribution after cleaning:")
+print(df_silver['issue_status'].value_counts())
+print()
+
+# Verificar se ainda ficou algo inconsistente, se a primeira checagem já resolveu todos os problemas.
+remaining_inconsistent = (df_silver['issue_status'].isin(['Done', 'Resolved']) & df_silver['timestamp_updated_at'].isna()).sum()
+print(f"Remaining inconsistent records: {remaining_inconsistent}")
+
+# Salva o arquivo final
+df_silver.to_json('2 - silver/silver_issues.json', 
+                  orient='records', 
+                  lines=True, 
+                  force_ascii=False)
+
+if os.path.exists('2 - silver/silver_issues.json'):
+    print("Arquivo salvo com sucesso!")
